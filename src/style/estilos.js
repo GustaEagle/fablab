453ea745e8833f6918779b14cb8f59ff8c90e931
Vinelo0 <113726354+Vinelo0@users.noreply.github.com>
2023-11-29 07:39:58 -0300

    Update estilos.js

--- src/style/estilos.js
+++ src/style/estilos.js
@@ -4,5 +4,7 @@ import AutorizacaoStyle from "./AutorizacaoStyle";
 import MaquinaStyle from "./MaquinaStyle";
 import HeaderStyle from "./HeaderStyle";
 import AdministracaoStyle from "./AdministracaoStyle";
+import ReservaStyle from "./ReservaStyle";
 
-export default { HomeStyle, LoginStyle, AutorizacaoAdmStyle: AutorizacaoStyle, MaquinaStyle, HeaderStyle, AutorizacaoAdmStyle: AdministracaoStyle}
+export default { HomeStyle, LoginStyle, AutorizacaoAdmStyle: AutorizacaoStyle, MaquinaStyle, HeaderStyle, AutorizacaoAdmStyle: AdministracaoStyle, ReservaStyle}
+
